fdc318846021dfb10eef8d15db490c91394fcd1b
DESKTOP-E5F373D\pc <rabinsiva207@gmail.com>
2021-10-08 18:04:29 +0530

    init

---
+++ upload.php
@@ -0,0 +1,29 @@
+
+<?php
+
+//upload.php
+
+if(isset($_POST['image']))
+{
+	$data = $_POST['image'];
+
+	
+
+	$image_array_1 = explode(";", $data);
+
+	
+
+	$image_array_2 = explode(",", $image_array_1[1]);
+
+	
+
+	$data = base64_decode($image_array_2[1]);
+
+	$image_name = 'upload/' . time() . '.png';
+
+	file_put_contents($image_name, $data);
+
+	echo $image_name;
+}
+
+?>
